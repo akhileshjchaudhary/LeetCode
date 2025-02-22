931c37d201d2726a195c4bbebf5e8dc9092eceeb
Akhilesh Chaudhary <131151427+akhileshjchaudhary@users.noreply.github.com>
2025-02-22 15:28:27 +0530

    Time: 11 ms (13.72%) | Memory: 47.4 MB (70.95%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -1,36 +1,52 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        if (root == null) return 0; // Edge case: Empty tree
-
         int sum = 0;
         int max = Integer.MIN_VALUE;
-        int level = 0, currentLevel = 1; // Start from level 1
+        int level = 0;
+        int currentLevel = 0;
 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         q.add(null);
-
         while (!q.isEmpty()) {
             TreeNode curr = q.poll();
-
             if (curr == null) {
-                level++; // Move to the next level
+                level++;
 
-                if (sum > max) { // Update max sum and corresponding level
+                if (max < sum) {
                     max = sum;
                     currentLevel = level;
                 }
 
-                if (!q.isEmpty()) {
+                if(!q.isEmpty()){
                     q.add(null);
-                    sum = 0;
                 }
+                
+                sum = 0;
+
             } else {
-                sum += curr.val;
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+                sum = sum + curr.val;
             }
         }
         return currentLevel;
