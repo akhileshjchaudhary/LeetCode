eaa5c7f6e39d9c68db8ce36f83c68b253a7edca4
Akhilesh Chaudhary <131151427+akhileshjchaudhary@users.noreply.github.com>
2026-02-14 16:09:21 +0530

    Time: 624 ms (12.27%) | Memory: 60.6 MB (5.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> mainList = new ArrayList<>();
-        Set<List<Integer>> set =new HashSet<>();
         Arrays.sort(nums);
-        for(int i=0; i<nums.length-2; i++){
-            int j=i+1; int k=nums.length-1;
-            while(j<k){
-                int ans = nums[i]+nums[j]+nums[k];
-                if(ans==0){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    set.add(list);
-                    j++;k--;
-                }else if(ans>0){
-                    k--;
+        Set<List<Integer>> mainList = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int first = i + 1;
+            int last = nums.length - 1;
+            while(first < last){
+                int sum = nums[i]+ nums[first] + nums[last];
+                if(sum == 0){
+                    ArrayList<Integer> list = new ArrayList<>();
+                    Collections.sort(list); list.add(nums[i]); list.add(nums[first]); list.add(nums[last]);
+                    if(!(mainList.contains(list))){
+                        mainList.add(new ArrayList<>(Arrays.asList(nums[i], nums[first], nums[last])));
+                    }
+                    first++;
+                }else if(sum > 0){
+                    last--;
                 }else{
-                    j++;
+                    first++;
                 }
             }
         }
-        mainList.addAll(set);
-        return mainList;
+        List<List<Integer>> List = new ArrayList<>(mainList);
+        return List;
     }
 }
